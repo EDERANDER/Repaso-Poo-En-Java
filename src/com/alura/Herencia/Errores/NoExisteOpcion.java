7ff79f7e6736f3407ab7e1c958d66430449770ab
EDERANDER <ea.quispe@est.unap.edu.pe>
2025-02-03 11:35:06 -0500

    documentacion de la clase de excepciones personalizadas

--- src/com/alura/Herencia/Errores/NoExisteOpcion.java
+++ src/com/alura/Herencia/Errores/NoExisteOpcion.java
@@ -3,10 +3,20 @@ package com.alura.Herencia.Errores;
 public class NoExisteOpcion extends RuntimeException {
     private String mensaje;
 
+    /**
+     *
+     * @param mensaje
+     * Constructor de nuestra Exception personalizada
+     */
     public NoExisteOpcion(String mensaje) {
         this.mensaje = mensaje;
     }
 
+    /**
+     * Metodo que sobreescribe a getMessage() de la clase RuntimeException
+     * RuntimeException es una clase con el tipo de excepciones no verificadas
+     * @return
+     */
     @Override
     public String getMessage(){
         return mensaje;
