a20a6302d996d2c750e90bf98fdd1f9962c73183
EDERANDER <ea.quispe@est.unap.edu.pe>
2025-02-03 11:29:51 -0500

    creando la documentacion de vehiculo

--- src/com/alura/Herencia/Vehiculo.java
+++ src/com/alura/Herencia/Vehiculo.java
@@ -1,7 +1,9 @@
 package com.alura.Herencia;
 
 abstract class Vehiculo {
-
+    /**
+     * Atributos de la clase abstracta de vehiculo
+     */
     private String marca;
     private String modelo;
     private String año;
@@ -10,7 +12,9 @@ abstract class Vehiculo {
      * @param marca
      * @param modelo
      * @param año
-     * Constructor de la clase Vehiculo que nos permite inicializar los atributos en las clases hijas
+     * Constructor de la clase Vehiculo que nos permite realizar
+     * la inicializacion de parametros de los atributos
+     * en las clases hijas, con el fin de crear un objeto nuevo.
      */
     public Vehiculo(String marca, String modelo, String año){
         this.marca = marca;
@@ -18,18 +22,34 @@ abstract class Vehiculo {
         this.año = año;
     }
 
+    /**
+     *
+     * @return la marca de Vehiculo
+     */
     public String getMarca() {
         return marca;
     }
 
+    /**
+     *
+     * @return el modelo del vehiculo
+     */
     public String getModelo() {
         return modelo;
     }
 
+    /**
+     *
+     * @return el año de fabrica del vehiculo
+     */
     public String getAño() {
         return año;
     }
 
+    /**
+     *Es un metodo sobreescrito de la clase Objecto especificamente toString
+     * @return no retorna nada porque es un metodo abstracto
+     */
     @Override
     public abstract String toString();
 }
